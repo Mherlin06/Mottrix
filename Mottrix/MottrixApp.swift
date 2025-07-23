1ed29e77d23396adcfbd8476ad2b1a9e56200630
Mherlin <mourice.hugues@gmail.com>
2025-07-23 12:54:19 +0200

    Initial Commit

---
+++ Mottrix/MottrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MottrixApp.swift
+//  Mottrix
+//
+//  Created by Hugues Mourice on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MottrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
